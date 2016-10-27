506e28c1847eaae0a0634b687490aadc3bd9fbe6
Kara Fulgum <karafulgum@gmail.com>
2016-10-27 16:00:35 -0500

    Created a mounted function

--- app/index.js
+++ app/index.js
@@ -4,6 +4,10 @@ import Vue from 'vue/dist/vue';
 const app = new Vue({
   el: '.app',
 
+  mounted() {
+    this.searchSeries('Hulk');
+  },
+
   methods: {
     searchSeries(series) {
       fetch('http://gateway.marvel.com/v1/public/series?limit=1&titleStartsWith=series&apikey=3d82878db752f59c47fdf37c7b4058f1')
