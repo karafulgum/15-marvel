d1c486240f9a48dee4bc3145af612baed7abaa26
Kara Fulgum <karafulgum@gmail.com>
2016-10-27 15:50:30 -0500

    Start a Vue Component

--- app/index.js
+++ app/index.js
@@ -1,1 +1,16 @@
 import 'whatwg-fetch';
+import Vue from 'vue/dist/vue';
+
+const app = new Vue({
+  el: '.app',
+
+  methods: {
+    searchSeries(series) {
+      fetch('http://gateway.marvel.com/v1/public/series?limit=1&titleStartsWith=series&apikey=3d82878db752f59c47fdf37c7b4058f1')
+            .then((r) => r.json())
+            .then((data) => {
+              this.seriesData = data;
+            });
+    },
+  },
+});
