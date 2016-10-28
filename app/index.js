56c9c0f86b59054b4a3859d52d222be2bcb1aa7f
Kara Fulgum <karafulgum@gmail.com>
2016-10-28 11:13:58 -0500

    Update HTML for Vue templating for sidebar

--- app/index.js
+++ app/index.js
@@ -1,8 +1,16 @@
 import 'whatwg-fetch';
 import Vue from 'vue/dist/vue';
 
+const apikey = '3d82878db752f59c47fdf37c7b4058f1';
+
 const app = new Vue({
   el: '.app',
+
+  data() {
+    return {
+      seriesData: null,
+    };
+  },
 
   mounted() {
     this.searchSeries('Hulk');
@@ -10,10 +18,10 @@ const app = new Vue({
 
   methods: {
     searchSeries(series) {
-      fetch('http://gateway.marvel.com/v1/public/series?limit=1&titleStartsWith=series&apikey=3d82878db752f59c47fdf37c7b4058f1')
+      fetch(`http://gateway.marvel.com/v1/public/series?limit=1&titleStartsWith=${series}&apikey=${apikey}`)
             .then((r) => r.json())
             .then((data) => {
-              this.seriesData = data;
+              this.seriesData = data.data.results[0];
             });
     },
   },
